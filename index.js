49c452d4f1428d43e20dce957d1152b61e96b437
Johan Castillo <jcjohan2707@gmail.com>
2021-02-12 13:27:53 -0400

    Chat real time finished

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const path = require('path')
 const express = require("express")
 const app = express()
+const SocketIO = require("socket.io")
+
 
 // Settings PORT
 app.set('port', process.env.PORT || 8080)
@@ -10,6 +12,23 @@ app.set('port', process.env.PORT || 8080)
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Start server
-app.listen(app.get('port'), () => {
+const server = app.listen(app.get('port'), () => {
     console.log('Server run port ', app.get('port'))
+})
+
+// Config SocketIO
+const io = SocketIO(server)
+
+// Websockets
+io.on('connection', socket => {
+    console.log("New conection", socket.id)
+
+    socket.on('chat:message', data => {
+        io.sockets.emit('chat:message', data)
+    })
+
+    socket.on('chat:typing', data => {
+        //console.log(data)
+        socket.broadcast.emit('chat:typing', data)
+    })
 })
--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,34 @@
-alert("Hellooooo")
+const socket = io()
+
+const message = document.getElementById('message')
+const username = document.getElementById('username')
+const btnSend = document.getElementById('send')
+const output = document.getElementById('output')
+const actions = document.getElementById('actions')
+
+
+btnSend.addEventListener('click', () => {
+    socket.emit('chat:message', {
+        username: username.value,
+        message: message.value
+    })
+
+    
+})
+
+message.addEventListener('keypress', () => {
+    socket.emit('chat:typing', username.value)
+})
+
+socket.on('chat:message', data => {
+    actions.innerHTML = ''
+    output.innerHTML += `<p>
+        <strong>${data.username}</strong>:
+        ${data.message}
+    </p>
+    `
+})
+
+socket.on('chat:typing', data => {
+    actions.innerHTML = `<p><em>${data} está escribindo</em></p>`
+})
